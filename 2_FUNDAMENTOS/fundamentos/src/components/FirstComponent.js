ea5a9f6b211d782ea20740ab63cd2b03db428d2d
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2023-03-08 12:49:18 -0300

    20 - comentários

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,10 @@
 const FirstComponent = () => {
-
+    //essa função faz isso
     return (
         <div>
+            {/* algum comentário */}
             <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
         </div>
     );
 
