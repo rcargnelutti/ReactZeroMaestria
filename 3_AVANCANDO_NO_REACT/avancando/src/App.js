47676db5862bece4fd951d16d06da3f66f8ea6df
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2023-03-09 11:28:10 -0300

    28 - Imagens públicas

---
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Avançando em React</h1>
+
+        {/* Imagem em public */}
+        <div>
+          <img src="/img1.jpg" alt="Paisagem" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
