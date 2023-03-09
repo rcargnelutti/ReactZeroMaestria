cfc6e816735bc68b3ee7d9bdfbb220bace5ce163
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2023-03-09 12:40:55 -0300

    38

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import City from './assets/city.jpg'
+import CarDetails from './components/CarDetails';
 import ConditionalRender from './components/CondidionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
@@ -24,7 +25,9 @@ function App() {
         <ManageData />
         <ListRender />
         <ConditionalRender />
+        {/* props */}
         <ShowUserName name="Rodrigo"/>
+        <CarDetails brand="VW" km={100000} color="Azul" />
       </header>
     </div>
   );
